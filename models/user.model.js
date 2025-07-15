1d541d9081db8e8af188b9b53642e621059fb3aa
AyushSmith00 <140578065+AyushSmith00@users.noreply.github.com>
2025-07-15 21:41:29 +0530

    second Commit

--- models/user.model.js
+++ models/user.model.js
@@ -4,18 +4,18 @@ const userSchema = new mongoose.Schema({
 
     username: {
         type: String,
-        require: true,
+        required: true,
         unique: true,
         lowercase: true,
     },
     email: {
         type: String,
-        require: true,
+        required: true,
         unique: true,
     },
     password: {
         type: String,
-        require: true,
+        required: true,
         unique: true,
     }
 
